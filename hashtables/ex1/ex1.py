12f25ab562d080b7453dbec56d55de4aec317038
Lance Smith <sirlancepants@gmail.com>
2020-06-07 17:43:15 -0500

    Completed mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,20 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # Create a dict with the index positions and weight values
+    # Look for pairs by looping through the weights, checking our dictionary for a number that is equal to the limit minus the current weight(target weight)
+    
+    cache = {}
 
+    for i in range(length):
+        cache[weights[i]] = i
+    
+    for i, w in enumerate(weights):
+        targetWeight = limit - w
+        if targetWeight in cache:
+            if i > cache[targetWeight]:
+                return (i, cache[targetWeight])
+            return (cache[targetWeight], i)
+           
     return None
+    
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,9 +3,19 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    cache = {}
+    result = []
+    for n in a:
+        cache[n] = n
+    
+    for n in a:
+        targetNum = 0
+        if n > 0:
+            targetNum = n * -1
+            if targetNum in cache:
+                result.append(n)
+    
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
